264b1456c95178aa7785a1032dad8c2ea1fb4992
Kottah02 <kottahst@gmail.Com>
2025-02-25 12:25:01 +0100

    V3

--- controlers/carte.controlers.go
+++ controlers/carte.controlers.go
@@ -1,11 +1,18 @@
 package controlers
 
 import (
+	"Groupietracker/services"
 	"Groupietracker/templates"
 	"net/http"
 )
 
 // créer une fonction controler qui permet de compléter la fonction route associé au meme template.
 func CarteControler(w http.ResponseWriter, r *http.Request) {
-	templates.ListTemp.ExecuteTemplate(w, "carte", nil)
+	responsData, responseCode, responseErr := services.GetYugiohData()
+	if responseErr != nil || responseCode != http.StatusOK {
+		http.Error(w, responseErr.Error(), http.StatusInternalServerError) // a changer par une redirection !
+		return
+	}
+
+	templates.ListTemp.ExecuteTemplate(w, "carte", responsData)
 }
---
+++ services/cartes.services.go
@@ -0,0 +1,50 @@
+package services
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"time"
+)
+
+type YugiohCard struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Type  string `json:"type"`
+	Level int    `json:"level"`
+	ATK   int    `json:"atk"`
+	DEF   int    `json:"def"`
+	Race  string `json:"race"`
+}
+
+type YugiohResponse struct {
+	Data []YugiohCard `json:"data"`
+}
+
+func GetYugiohData() (YugiohResponse, int, error) {
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	req, err := http.NewRequest(http.MethodGet, "https://db.ygoprodeck.com/api/v7/cardinfo.php", nil)
+	if err != nil {
+		return YugiohResponse{}, http.StatusInternalServerError, err
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return YugiohResponse{}, http.StatusInternalServerError, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return YugiohResponse{}, resp.StatusCode, errors.New("la requête a échoué avec le code : " + resp.Status)
+	}
+
+	var yugiohResponse YugiohResponse
+	if err := json.NewDecoder(resp.Body).Decode(&yugiohResponse); err != nil {
+		return YugiohResponse{}, http.StatusInternalServerError, err
+	}
+
+	return yugiohResponse, http.StatusOK, nil
+}
